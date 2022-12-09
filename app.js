c18bb7d323e82e912314b92a4b9ad4361111e13e
J. Jackson <jjackson11211@gmail.com>
2022-12-09 09:55:31 -0500

    JS
    Array of choices
    For loop
    Event listeners
    I??
    Length property
    innerHTML
    appendChild

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const resultDisplay = document.createElement('h1');
 const gameGrid = document.getElementById('game');
 
 gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
+
+const choices = ['rock', 'paper', 'scissors']
+let userChoice
+
+const handleClick = () => {
+    // console.log('clicked')
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+}
+
+// create three buttons, grab button with an id, give the id choices, event listener, call function handleClick
+for (let i=0; i <choices.length; i++) {
+    const button =  document.createElement ('button')
+    button.id = choices[i] // you can delete this if you want to use e.target.HTML
+    button.innerHTML = choices[i]
+    button.addEventListener('click', handleClick)
+    gameGrid.appendChild(button)
+}
+
